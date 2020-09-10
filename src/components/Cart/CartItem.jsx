2c3124286c4c1ab9cf912c00030bc63a5aba657e
SvShok <svshok1@gmail.com>
2020-09-10 19:22:38 +0300

    addCartIncrementAndInsertEvents

--- src/components/Cart/CartItem.jsx
+++ src/components/Cart/CartItem.jsx
@@ -1,30 +1,52 @@
 import React from 'react';
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
 import { Link } from "react-router-dom";
-import ListItemText from "@material-ui/core/ListItemText";
-import List from "@material-ui/core/List";
 import {makeStyles} from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Input from '@material-ui/core/Input';
+import { OutlinedInput } from '@material-ui/core';
+import Button from "@material-ui/core/Button";
 
 const useStyles = makeStyles((theme) => ({
     container:{
-        justifyContent: 'space-between'
+        borderBottom: `1px solid ${theme.palette.primary.main}`,
+    },
+    cartQuantity:{
+        border: `1px solid ${theme.palette.primary.main}`,
+    },
+    inputCount: {
+      width: "2rem"
     }
 }));
 
-const CartItem = ({cartData, addToCart}) =>{
+
+const CartItem = ({cartData, addToCart, removeFromCart, decCartCount, InputChangeCartCount}) =>{
     const {id, title, img, price, info, inCart, count, total} = cartData;
-    debugger
     const classes = useStyles();
+
+    const handleInputOnChange = event => {
+        const { value } = event.target;
+        InputChangeCartCount({cartData, value});
+    };
     return (
         <>
-        <ListItem className={classes.container}>
-            <div><Link to="#"><img src={img} width="189" alt={title}/></Link></div>
-            <div>{title}</div>
-            <div>{count}</div>
-            <div>{price}</div>
-        </ListItem>
-        <Divider />
+        <Grid item container
+              alignItems="center"
+              spacing={2}
+              className={classes.container}
+        >
+            <Grid item xs={6} md={3}><Link to="#"><img src={img} width="189" alt={title}/></Link></Grid>
+            <Grid item xs={6} md={3}>{title}</Grid>
+            <Grid item xs={6} md={3}>
+                <div><button onClick={()=> decCartCount(cartData)}>-</button>
+                    <Input value={count} className={classes.inputCount} onChange={handleInputOnChange} type='number'/>
+                    <button onClick={()=> addToCart(cartData)}>+</button>
+                </div></Grid>
+            <Grid item xs={6} md={3}>{price} грн.
+                <Button variant="contained" color="primary" onClick={()=>removeFromCart(cartData)}>
+                Delete
+            </Button>
+            </Grid>
+        </Grid>
         </>
     );
 };
